39037a0c09d1a9f86838673210014a9f191d9e1f
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-02-20 17:12:11 +0900

    fix: NewsCardを移植

---
+++ app/_features/NewsCardList/NewsCard/NewsCard.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { NewsCard } from "./NewsCard";
+import NewsImage from "@/public/News/NewsImage.png";
+
+const meta: Meta<typeof NewsCard> = {
+  title: "features/NewsCardList/NewsCard",
+  component: NewsCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof NewsCard>;
+
+export const Default: Story = {
+  args: {
+    tag: "部活動",
+    date: '2018-04-04',
+    title: 'aaaaaaaaaaaa',
+    link: "",
+    image: NewsImage
+  },
+};
