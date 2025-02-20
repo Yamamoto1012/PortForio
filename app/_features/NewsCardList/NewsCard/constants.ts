d37d1e39cb3f854b9e0f9343303720d1e3d2738d
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-02-20 18:31:08 +0900

    feat: NewsCardListの変数追加

---
+++ app/_features/NewsCardList/NewsCard/constants.ts
@@ -0,0 +1,9 @@
+export const NEWS_CARD_TAGS = [
+  { id: 0, label: "全て" },
+  { id: 1, label: "私生活" },
+  { id: 2, label: "音楽" },
+  { id: 3, label: "ゲーム" },
+  { id: 4, label: "プログラミング" },
+  { id: 5, label: "動画" },
+  { id: 6, label: "イラスト" },
+] as const;
